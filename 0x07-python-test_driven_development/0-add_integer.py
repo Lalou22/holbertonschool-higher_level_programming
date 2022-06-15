daf2a240d58aded60f1542cd2f8d8d521bee6d5b
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-06-15 17:11:25 -0500

    Task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -8,17 +8,10 @@ if they are float.
 
 
 def add_integer(a, b=98):
-    """Function that adds 2 integers.
-
-    Args:
-        a: First int.
-        b: Second int.
-
-    Raises:
-        TypeError: If a or b are not int or float.
-
-    Returns:
-        The sum of a and b.
+    """
+    Function that adds 2 integers. a and b must be integers or floats,
+    otherwise raise a TypeError exception with the message a must be an
+    integer or b must be an integer
     """
     if isinstance(a, (int, float)) is False:
         raise TypeError('a must be an integer')
