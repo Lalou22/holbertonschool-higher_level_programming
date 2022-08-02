79030dcacbb8baad85ee6d2128b54402b20c05ab
Lalou22 <eduardo.roddelange@anahuac.mx>
2022-08-02 09:32:11 -0500

    Task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+console.log(factorial(parseInt(process.argv[2])));
+function factorial (x) {
+  if (isNaN(x) || x === 1) {
+    return (1);
+  } else {
+    return (x * factorial(x - 1));
+  }
+}
